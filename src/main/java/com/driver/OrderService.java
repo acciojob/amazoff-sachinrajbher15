9f0b33bf18c72d0659b31570c578fc7d854c2d2e
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-06-01 21:45:05 +0530

    Update OrderService.java

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -19,47 +19,70 @@ public class OrderService {
         orderRepository.savePartner(partnerId);
     }
 
-    public void createOrderPartnerPair(String orderId, String partnerId){
-        orderRepository.saveOrderPartnerMap(orderId, partnerId);
+    public void addOrderPartnerPair(String orderId, String partnerId){
+        orderRepository.addOrderPartnerMap(orderId, partnerId);
     }
 
     public Order getOrderById(String orderId){
-        return orderRepository.findOrderById(orderId);
+        return orderRepository.getOrderById(orderId);
     }
 
     public DeliveryPartner getPartnerById(String partnerId){
-        return orderRepository.findPartnerById(partnerId);
+        return orderRepository.getPartnerById(partnerId);
     }
 
-    public Integer getOrderCountByPartnerId(String partnerId){
-        return orderRepository.findOrderCountByPartnerId(partnerId);
+    public int getOrderCountByPartnerId(String partnerId){
+        return orderRepository.getOrderCountByPartnerId(partnerId);
     }
 
     public List<String> getOrdersByPartnerId(String partnerId){
-        return orderRepository.findOrdersByPartnerId(partnerId);
+        return orderRepository.getOrdersByPartnerId(partnerId);
     }
 
     public List<String> getAllOrders(){
-        return orderRepository.findAllOrders();
+        return orderRepository.getAllOrders();
+    }
+    
+    public void deletePartnerById(String partnerId){
+        orderRepository.deletePartnerById(partnerId);
     }
 
-    public void deletePartner(String partnerId){
-        orderRepository.deletePartner(partnerId);
+    public void deleteOrderById(String orderId){
+        orderRepository.deleteOrderById(orderId);
     }
 
-    public void deleteOrder(String orderId){
-        orderRepository.deleteOrder(orderId);
+    public int getCountOfUnassignedOrders(){
+        return orderRepository.getCountOfUnassignedOrders();
     }
 
-    public Integer getCountOfUnassignedOrders(){
-        return orderRepository.findCountOfUnassignedOrders();
-    }
+    public int getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId){
+        //        String s1 = String.valueOf(time.charAt(0) + time.charAt(1));
+        //        String s2 = String.valueOf(time.charAt(3) + time.charAt(4));
+        //        int hh = Integer.valueOf(s1);
+        //        int mm = Integer.valueOf(s2);
 
-    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId){
-        return orderRepository.findOrdersLeftAfterGivenTimeByPartnerId(time, partnerId);
+        String arr[] = time.split(":");
+        int hh = Integer.parseInt(arr[0]);
+        int mm = Integer.parseInt(arr[1]);
+        int timeInt = (hh*60)+mm;
+        int count = orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(timeInt, partnerId);
+        return count;
     }
 
     public String getLastDeliveryTimeByPartnerId(String partnerId){
-        return orderRepository.findLastDeliveryTimeByPartnerId(partnerId);
+        int timeInt = orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+        int hh = timeInt/60;
+        int mm = timeInt%60;
+        String HH = String.valueOf(hh);
+        if(HH.length()==1){
+            HH = '0' + HH;
+        }
+        String MM = String.valueOf(mm);
+        if(MM.length()==1){
+            MM = '0'+MM;
+        }
+
+        String time = HH + ":" + MM;
+        return time;
     }
 }
